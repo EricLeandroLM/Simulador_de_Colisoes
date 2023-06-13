dfe839601d5411394d5c109d5438a1189e4e9cfd
karla-easterlini <135055293+karla-easterlini@users.noreply.github.com>
2023-06-13 15:04:04 -0300

    Update Projeto-bolas.py

--- Projeto-bolas.py
+++ Projeto-bolas.py
@@ -46,7 +46,7 @@ def checarColisao(ball1, ball2):
         return False
     
 
-for j in range(0, 1000): #Função que cria as bolinhas aleatóriamente, faz elas mexerem
+for j in range(0, 50): #Função que cria as bolinhas aleatóriamente, faz elas mexerem
     balls.append(Ball(2, rand.choice(cores), vector(rand.randrange(-20, 20), rand.randrange(-20, 20), rand.randrange(-20, 20)), vector(rand.randrange(-100, 100), rand.randrange(-100, 100), rand.randrange(-100, 100)), 40, j))
 dt = 0.08 #velocidade da verificação de cada colisão nos frames ### CORRIGIR
 print(balls)
